76f14a8c9d8ce6d69d202f2165ce7df4fa257795
Hans Reichgelt <hreichgelt06@gmail.com>
2022-06-22 16:31:26 -0400

    adjusting consts adding push and adjusting getters and setters it is working

--- server.js
+++ server.js
@@ -1,80 +1,66 @@
 // Utilizing example from class
-const express = require('express');
-const path = require('path');
-const fs = require('fs');
+const express = require("express");
+const path = require("path");
+const fs = require("fs");
 const PORT = process.env.PORT || 3001;
-const database = require("./db/db.json");
-const uuid = require('./helpers/uuid.js');
-
-
-
+const notes = require("./db/db.json");
+const uuid = require("./helpers/uuid.js");
 const app = express();
 
 // Middleware for parsing application/json and urlencoded data
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 // need app.get for both html files? adding just in case
 app.get("/", (req, res) =>
-    res.sendFile(path.join(__dirname, "/public/index.html"))
+  res.sendFile(path.join(__dirname, "/public/index.html"))
 );
 
 app.get("/notes", (req, res) =>
-    res.sendFile(path.join(__dirname, "/public/notes.html"))
+  res.sendFile(path.join(__dirname, "/public/notes.html"))
 );
 
-// GET Route for retrieving all the tips - might need something like this for |||||| also could use readandappend
-// app.get('/api/tips', (req, res) => {
-//     console.info(`${req.method} request received for tips`);
-//     readFromFile('./db/tips.json').then((data) => res.json(JSON.parse(data)));
-//   });
-
-
-// need to writefile? writefilesync?
+app.get("/api/notes", (req, res) => {
+  res.sendFile(path.join(__dirname, "./db/db.json"));
+});
 
 app.get("/api/notes", (req, res) => {
-    console.info(`${req.method} request received to get note`);
-    return res.json(database);
+  console.info(`${req.method} request received for notes`);
+  res.json(notes);
 });
 
 // posting notes to database
-app.post(database, (req, res) => {
-    console.info(`${req.method} request received to add note`)
-
-// look at activity 21 read and append!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-
-    const {title, text} = req.body; 
-
-    if (title && text) {
-       const newNote = {
-          title,
-          text, 
-          id: uuid(),
-        };
-
-const noteString = JSON.stringify(newNote, null, 2);
+app.post("/api/notes", (req, res) => {
+  // readFromFile(database).then(data)
+  const { title, text } = req.body;
+  if (title && text) {
+    const newNote = {
+      title,
+      text,
+      id: uuid(),
+    };
+    notes.push(newNote);
+    const noteString = JSON.stringify(notes, null, 2);
     // follow lesson 19
-    fs.writeFile(`./db/${newNote.response}.json`, noteString, (err) => 
-    err
+    fs.writeFile('./db/db.json', noteString, (err) =>
+      err
         ? console.error(err)
-        : console.log(
-            `Note for ${newNote.title} has been written to JSON file`
-        )
-);
+        : console.log(`Note for ${newNote.title} has been written to JSON file`)
+    );
     const response = {
-        status: 'success',
-        body: newNote,
+      status: "success",
+      body: newNote,
     };
 
     console.log(response);
     res.status(201).json(response);
-} else {
-    res.status(500).json('Error in posting note');
-}
+  } else {
+    res.status(500).json("Error in posting note");
+  }
 });
 
 // event listener always at the bottom!~ Might as well create now
 app.listen(PORT, () => {
-    console.log(`Example app listening at http://localhost:${PORT}`);
-  });
+  console.log(`Example app listening at http://localhost:${PORT}`);
+});
